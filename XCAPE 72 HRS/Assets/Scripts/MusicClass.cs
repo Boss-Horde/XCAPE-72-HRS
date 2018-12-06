0e5b1898dd2f67ec2309b4dab0f65d90ccf79733
Dan <31317901+dghazvini@users.noreply.github.com>
2018-12-06 17:10:55 -0600

    Added Music, need to fix looping issue

---
+++ XCAPE 72 HRS/Assets/Scripts/MusicClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicClass : MonoBehaviour {
+
+    public AudioSource audioSource;
+
+    private void Awake() {
+        DontDestroyOnLoad(transform.gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic() {
+        if (audioSource.isPlaying)
+            return;
+        audioSource.Play();
+    }
+
+    public void StopMusic() {
+        audioSource.Stop();
+    }
+}

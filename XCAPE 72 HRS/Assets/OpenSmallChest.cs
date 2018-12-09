d28f11a81cda9d8d42e5ec8ff80609d2593fd838
Erwin Herrera <Erwin.Herrera97@gmail.com>
2018-12-08 22:47:47 -0600

    Updating small chest script
    -Changed to where you must enter E to solve riddles for opening chest
    -Still testing

--- XCAPE 72 HRS/Assets/OpenSmallChest.cs
+++ XCAPE 72 HRS/Assets/OpenSmallChest.cs
@@ -2,44 +2,96 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
 
-public class OpenSmallChest : MonoBehaviour {
+public class OpenSmallChest : MonoBehaviour
+{
+    private RigidbodyFirstPersonController firstPersonController;
 
     public GameObject riddlePanel;
     bool onTrigger = false;
     bool openedChest = false;
+    bool solvingRiddle = false;
     InputField input;
 
     private void Start()
     {
+        firstPersonController = FindObjectOfType<RigidbodyFirstPersonController>();
         riddlePanel.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.LogWarning("TRIGGER ENTERED");
+
         if (!openedChest)
         {
-
-            Cursor.visible = !Cursor.visible;
             if (other.CompareTag("Player"))
             {
                 onTrigger = true;
-                riddlePanel.SetActive(true);
-                input = riddlePanel.GetComponentInChildren<InputField>();
-                input.ActivateInputField();
-                input.Select();
             }
         }
+
+        ////Cursor.visible = !Cursor.visible;
+        //if (other.CompareTag("Player"))
+        //{
+        //    onTrigger = true;
+        //    riddlePanel.SetActive(true);
+        //    input = riddlePanel.GetComponentInChildren<InputField>();
+        //    input.ActivateInputField();
+        //    input.Select();
+        //}
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
+        //if (other.CompareTag("Player"))
+        //{
+        //}
+        onTrigger = false;
+    }
 
-            riddlePanel.SetActive(false);
+    private void OnGUI()
+    {
+        if (onTrigger)
+        {
+            GUI.Box(new Rect(0, 0, 200, 25), "Press 'E' to open chest");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                solvingRiddle = true;
+                onTrigger = false;
+            }
+        }
+
+        if (solvingRiddle)
+        {
+            riddlePanel.SetActive(true);
+            input = riddlePanel.GetComponentInChildren<InputField>();
+            input.ActivateInputField(); 
+            input.Select();
+
+            //Cursor.visible = !Cursor.visible;
+            firstPersonController.enabled = false;
+            GUI.Box(new Rect(50, 400, 200, 25), "Press 'Tab' to give up solving");
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                Debug.Log("1 exit pressed");
+                firstPersonController.enabled = true;
+
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+
+                solvingRiddle = false;
+
+                riddlePanel.SetActive(false);
+            }
         }
     }
+
 
     public void CheckInputString()
     {
